32a591f4ae2e1a861a83329edcbaa805cfeb909a
knusprigerschweinsbraten <stefan.pachler@gmail.com>
2022-04-05 07:35:56 +0200

    Update mydevenv.bat

--- bin/mydevenv.bat
+++ bin/mydevenv.bat
@@ -1,2 +1,2 @@
 @echo off
-docker run -it --rm --net host -v "%UserProfile%"/.kube/:/root/.kube/ -v "%cd%":/work -w /work mykubeenv:latest
+docker run -it --rm --net host -v "%UserProfile%"/.kube/:/root/.kube/ -v "%cd%":/work -w /work mydevenv:latest
